ee2f98f44fec6feaa5bacdd86bb7dc165126e12e
0ga0takash1 <ogawatakafumi2814@gmail.com>
2021-03-07 04:36:11 +0900

    リニューアル対応

--- qiita_script.js
+++ qiita_script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add lgtms-link
 // @namespace    https://greasyfork.org/users/684688
-// @version      0.1
+// @version      0.2
 // @description  Qiitaの自分のアイコンのドロップアウト欄に「LGTMした記事一覧」を追加します。
 // @author       0ga0takash1
 // @match        https://qiita.com/*
@@ -11,19 +11,21 @@
 {
     // class名が「st-Header_dropdown」であるものの中のaタグをリストで収集
     const links = Array.from(
-        document.querySelectorAll(".st-Header_dropdown > a")
+        document.querySelectorAll(".st-RenewalHeader_dropdown > a")
     );
+
+    // usernameを検知する用
     const my_page_url = links.find((element) => element.innerText === "マイページ").href;
 
     // リストから一つ前の要素を検出し、before_elementに代入
     const before_element = links.find(
-        (element) => element.innerText === "編集リクエスト一覧"
+        (element) => element.innerText === "ストックした記事"
     );
 
     // 挿入する要素lgtmsを作る
     const lgtms = document.createElement("a");
     lgtms.href = `${my_page_url}/lgtms`;
-    lgtms.className = "st-Header_dropdownItem";
+    lgtms.className = "st-RenewalHeader_dropdownItem";
     lgtms.innerHTML = "LGTMした記事一覧";
 
     // 挿入する
--- qiita_script_English.js
+++ qiita_script_English.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add lgtms-link_English
 // @namespace    https://greasyfork.org/users/684688
-// @version      0.1
+// @version      0.2
 // @description  Qiitaの自分のアイコンのドロップアウト欄に「LGTMした記事一覧」を追加します。
 // @author       0ga0takash1
 // @match        https://qiita.com/*
@@ -11,7 +11,7 @@
 {
     // class名が「st-Header_dropdown」であるものの中のaタグをリストで収集
     const links = Array.from(
-        document.querySelectorAll(".st-Header_dropdown > a")
+        document.querySelectorAll(".st-RenewalHeader_dropdown > a")
     );
 
     // usernameを検知する用
@@ -19,13 +19,13 @@
 
     // リストから一つ前の要素を検出し、before_elementに代入
     const before_element = links.find(
-        (element) => element.innerText === "Edit Requests"
+        (element) => element.innerText === "Stock Articles"
     );
 
     // 挿入する要素lgtmsを作る
     const lgtms = document.createElement("a");
     lgtms.href = `${my_page_url}/lgtms`;
-    lgtms.className = "st-Header_dropdownItem";
+    lgtms.className = "st-RenewalHeader_dropdownItem";
     lgtms.innerHTML = "My LGTM items";
 
     // 挿入する
